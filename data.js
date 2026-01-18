00be9f7a38e0bc41178acea447c669a62bc72a7f
rafaelsilcunha <rafaelsilcunha@gmail.com>
2026-01-18 02:01:21 +0000

    Added main menu container

--- data.js
+++ data.js
@@ -1,23 +1,26 @@
-const menuArray = [
+export const menuArray = [
     {
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
         id: 0,
         price: 14,
-        emoji: "🍕"
+        emoji: "🍕",
+        image: "./images/pizzaimg.png"
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
         emoji: "🍔",
-        id: 1
+        id: 1,
+        image: "./images/burgerimg.png"
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
-        id: 2
+        id: 2,
+        image: "./images/beerimg.png"
     }
 ]
--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-import { menuArray } from "./data";
+import { menuArray } from "./data.js";
+const menuItems = document.querySelector(".menu-items")
+const checkoutSection = document.querySelector(".checkout-Section")
+
+function renderMenu(){
+    menuItems.innerHTML = menuArray.map(({name, ingredients, id, price, emoji, image}) => {
+    return `<div class="item-container">
+                <div> 
+                    <img src="${image}" class="item-img">
+                </div>
+                <div class="item-description-container">
+                    <p class="item-title">${name}</p>
+                    <p class="item-description">${ingredients.join(", ")} </p>
+                    <p class="item-price">$${price}</p>
+                </div>
+                <div class= "add-btn-div">
+                    <button class="add-btn" data-id = ${id}>+</button>
+                </div>
+            </div>`
+    }).join("")
+}
+
+renderMenu()
+
+const selectedItems = []
+
+function renderOrder() {
+    checkoutSection.innerHTML = `
+        <div class="checkout-Section">
+            <p>Your Order</p>
+
+        </div>
+    `
+}
+
+const addBtn = document.getElementsByClassName("add-btn")
+addBtn.addEventListener("click", function(event){
+    console.log(event.target.dataset.id)
+    menuArray.forEach(function(food){
+        if (event.target.dataset.id == food.id){
+            selectedItems.push(food.name)
+            console.log(selectedItems)
+        }
+    })
+})
+
+
