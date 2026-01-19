3055b55ca37057d2f784ef4a14919629de42d3cc
rafaelsilcunha <rafaelsilcunha@gmail.com>
2026-01-19 00:55:37 +0000

    updated js and checkout css
    added js code to recognize what food item was clicked
    added CSS for the checkout section

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { menuArray } from "./data.js";
 const menuItems = document.querySelector(".menu-items")
 const checkoutSection = document.querySelector(".checkout-Section")
 
+
+
 function renderMenu(){
     menuItems.innerHTML = menuArray.map(({name, ingredients, id, price, emoji, image}) => {
     return `<div class="item-container">
@@ -14,7 +16,7 @@ function renderMenu(){
                     <p class="item-price">$${price}</p>
                 </div>
                 <div class= "add-btn-div">
-                    <button class="add-btn" data-id = ${id}>+</button>
+                    <button class="add-btn" data-id = "${id}">+</button>
                 </div>
             </div>`
     }).join("")
@@ -22,11 +24,11 @@ function renderMenu(){
 
 renderMenu()
 
-const selectedItems = []
+const selectedOrder = []
 
 function renderOrder() {
     checkoutSection.innerHTML = `
-        <div class="checkout-Section">
+        <div class="food-order-item">
             <p>Your Order</p>
 
         </div>
@@ -34,14 +36,19 @@ function renderOrder() {
 }
 
 const addBtn = document.getElementsByClassName("add-btn")
-addBtn.addEventListener("click", function(event){
-    console.log(event.target.dataset.id)
-    menuArray.forEach(function(food){
-        if (event.target.dataset.id == food.id){
-            selectedItems.push(food.name)
-            console.log(selectedItems)
-        }
+
+function getFoodObj (foodId){
+    const targetObj = menuArray.find(function(food){
+        return food.id === Number(foodId)
     })
+    selectedOrder.push(targetObj)
+    console.log(selectedOrder)
+}
+
+document.addEventListener("click", function(e){
+    if(e.target.dataset.id){
+        getFoodObj(e.target.dataset.id)
+    }
 })
 
 
